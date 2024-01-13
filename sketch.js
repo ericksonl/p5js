794650f629b1e07976a97398542cb6240d53d2c0
Liam <liamerickson20@gmail.com>
2024-01-13 01:08:38 -0800

    Adding color

--- sketch.js
+++ sketch.js
@@ -3,22 +3,33 @@ var y = 450
 var dx = 100
 var dy = 100
 speed = 1
+const Y_AXIS = 1;
+const X_AXIS = 2;
+let b1, b2, c1, c2;
 
 function setup() {
   createCanvas(1800, 900);
-  background(0)
+  background('#005073')
+
+    // Define colors
+    b1 = color(255);
+    b2 = color(0);
+    c1 = color(204, 102, 0);
+    c2 = color(0, 102, 153);
+  
+    // setGradient(50, 90, 540, 80, c1, c2, Y_AXIS);
+    // setGradient(50, 190, 540, 80, c2, c1, X_AXIS);
 }
 
-function draw() {
-  frameRate(60);
-  stroke(255)
+function draw() {  
+  stroke('#71c7ec')
   strokeWeight(1)
-  fill(0)
+  fill('#107dac')
   ellipse(x, y, dx, dy)
 
 
-  x = random(x - 10, x + 10)
-  y = random(y - 10, y + 10)
+  x = random(x - 15, x + 15)
+  y = random(y - 15, y + 15)
 
   if (dx >= 100 || dy >= 100) {
     speed = -1
@@ -41,4 +52,26 @@ function draw() {
     y = y + 10
   }
 
+}
+
+function setGradient(x, y, w, h, c1, c2, axis) {
+  noFill();
+
+  if (axis === Y_AXIS) {
+    // Top to bottom gradient
+    for (let i = y; i <= y + h; i++) {
+      let inter = map(i, y, y + h, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(x, i, x + w, i);
+    }
+  } else if (axis === X_AXIS) {
+    // Left to right gradient
+    for (let i = x; i <= x + w; i++) {
+      let inter = map(i, x, x + w, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(i, y, i, y + h);
+    }
+  }
 }
