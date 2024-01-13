ab45ce4eb648efda4bf85daf6c1309c0951134ab
Liam <liamerickson20@gmail.com>
2024-01-12 23:34:05 -0800

    init commit

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(400, 300);
+}
+
+function draw() {
+  background('#000000') 
+  
+  rectMode(CENTER)
+
+  fill(0, 0, 255)
+  stroke(0, 255, 0)
+  rect(200, 150, 150, 150)
+
+
+
+
+  ellipse(150, 250, 100, 75)
+
+  fill(255, 0, 0)
+  stroke(255)
+}
