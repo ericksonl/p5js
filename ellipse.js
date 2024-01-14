d61d752cd3472fa4408eb0fb743099533bee3707
Liam <liamerickson20@gmail.com>
2024-01-14 15:08:13 -0800

    Adding slider capabilities to change properties

--- ellipse.js
+++ ellipse.js
@@ -1,27 +1,39 @@
 class Ellipse {
-    constructor(x, y, diameter) {
+    constructor(x, y, diameter, opacity, scatter, steps) {
         this.x = x
         this.y = y
         this.diameter = diameter
-        this.steps = 0
+        this.opacity = opacity
+        this.scatter = scatter
+        this.steps = steps
         this.color = {
             r: random(50, 255),
             g: random(50, 255),
             b: random(50, 255),
+            a: this.opacity,
             rgb: 1
         }
     }
 
     display() {
-        fill(this.color.r, this.color.g, this.color.b)
+        fill(this.color.r, this.color.g, this.color.b, this.color.a)
         noStroke()
         ellipse(this.x, this.y, this.diameter, this.diameter)
     }
 
     move() {
-        this.x += random(-100, 100);
-        this.y += random(-100, 100);
-        this.steps++
+        this.x += random(-this.scatter, this.scatter);
+        this.y += random(-this.scatter, this.scatter);
+        if (this.x > width - this.diameter / 2 || this.x < this.diameter / 2) {
+            this.x = constrain(this.x, this.diameter / 2, width - this.diameter / 2);
+            console.log("Bounce")
+        }
+
+        if (this.y > height - this.diameter / 2 || this.y < this.diameter / 2) {
+            this.y = constrain(this.y, this.diameter / 2, height - this.diameter / 2);
+            console.log("Bounce")
+        }
+        this.steps--
     }
 
     changeColor() {
@@ -33,5 +45,5 @@ class Ellipse {
             this.color.rgb *= -1;
         }
     }
-    
+
 }
--- sketch.js
+++ sketch.js
@@ -1,47 +1,73 @@
-var boundsX = 0
-var boundsY = 0
-var rWidth = 800
-var steps = 0
 const ellipses = []
 let myCanvas
+let opacity, ballSize, scatterSize
 
 function setup() {
-  myCanvas = createCanvas(800, 800)
-  background(random(0,255), random(0,255), random(0,255))
-
-  console.log(ellipses.length)
+  
+  myCanvas = createCanvas(windowWidth * 0.5, windowHeight * 0.5);
+  background(random(0, 255), random(0, 255), random(0, 255))
 
   noFill()
-  strokeWeight(2)
-  rect(boundsX, boundsY, rWidth, rWidth);
+  stroke(0)
+  strokeWeight(4)
+  rect(0, 0, width, height);
 }
 
 function draw() {
   for (i = 0; i < ellipses.length; i++) {
     ellipses[i].display()
     // ellipse2.display()
-
-    if (
-      ellipses[i].x >= boundsX + ellipses[i].diameter &&
-      ellipses[i].x <= boundsX + rWidth - ellipses[i].diameter &&
-      ellipses[i].y >= boundsY + ellipses[i].diameter &&
-      ellipses[i].y <= boundsY + rWidth - ellipses[i].diameter && ellipses[i].steps < 500
-    ) {
+    if (ellipses[i].steps > 1) {
+      console.log(ellipses[i].steps)
       ellipses[i].move()
       ellipses[i].changeColor()
+    } else {
+      ellipses.splice(i, 1)
     }
   }
+}
 
-  if (steps < 500) {
-    steps++
-  }
+
+function spawnEllipse() {
+  ellipses.push(new Ellipse(random(400, 500), random(375, 475), getSize(), getOpacity(), getScatter(), getSteps()))
 }
 
 function saveImg() {
-  savedImg = myCanvas.get(boundsX, boundsY, rWidth, rWidth)
+  savedImg = myCanvas.get(0, 0, width, height)
   savedImg.save('suck', 'png')
 }
 
-function spawnEllipse() {
-  ellipses.push(new Ellipse(random(400, 500), random(375, 475), random(30, 60)))
+function clearCanvas() {
+  clear()
+  ellipses.length = 0
+  background(random(0, 255), random(0, 255), random(0, 255))
+  
+  noFill()
+  stroke(0)
+  strokeWeight(4)
+  rect(0, 0, width, height);
+}
+
+function getOpacity() {
+  opacity = parseInt(document.getElementById("opacityRange").value)
+  document.getElementById("opacity").innerHTML = "Opacity: " + Math.floor(opacity / 255 * 100) + "%"
+  return (opacity)
+}
+
+function getSize() {
+  ballSize = parseInt(document.getElementById("sizeRange").value)
+  document.getElementById("ballSize").innerHTML = "Ball Size: " + ballSize
+  return (ballSize)
+}
+
+function getScatter() {
+  scatterSize = parseInt(document.getElementById("scatterRange").value)
+  document.getElementById("scatter").innerHTML = "Scatter: " + scatterSize + "px"
+  return (scatterSize)
+}
+
+function getSteps() {
+  generations = parseInt(document.getElementById("stepsRange").value)
+  document.getElementById("steps").innerHTML = "Generations: " + generations
+  return (generations)
 }
